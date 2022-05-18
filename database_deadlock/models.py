d1576108b39dc04a709f9dbd289762124927e863
Yamao Maoen <yamaomaoen@gmail.com>
2022-05-18 16:37:24 +0200

    Upgrade to Django 4

--- database_deadlock/models.py
+++ database_deadlock/models.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 
 class Deadlock(models.Model):
